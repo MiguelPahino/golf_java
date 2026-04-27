71467235f2ac56664b78e03fb1e05103353dc659
MiguelPhino <mpahinogutierrez@gmail.com>
2026-04-27 14:14:55 +0200

    feat(ScoreCard): :sparkles: Añadir nuevos métodos al scorecard

--- src/main/java/edu/teamrocket/golfstrokes/ScoreCard.java
+++ src/main/java/edu/teamrocket/golfstrokes/ScoreCard.java
@@ -1,6 +1,7 @@
 package edu.teamrocket.golfstrokes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,9 @@ public class ScoreCard {
 
     private Course playersCourse;
 
-    public ScoreCard(){}
+    public ScoreCard(){
+        
+    }
 
     public void setPlayerA(Player playerA) {
         this.playerA = Optional.of(playerA);
@@ -32,8 +35,35 @@ public class ScoreCard {
         this.playerD = Optional.of(playerD);
     }
 
-    byte[] getplayersCourse(Player player){
+    void setPlayersCourse(Course course ){
+        this.playersCourse = course;
+    }
+
+    public List<Player> getPlayers(){
+        return Arrays.asList(playerA,playerB,playerC,playerD)
+        .stream()
+        .filter(Optional::isPresent)
+        .map(Optional::get).toList();
+
+    }
+
+    byte[] getPlayersCourse(Player player){
         return playersCourse.getPlayerCourse(player);
+    }
+
+    void addHoles(Byte[] holesPar){
+        byte holeNumber = 1;
+        for ( Byte par : holesPar){
+            this.holes.add(new Hole(holeNumber, par));
+        }
+    }
+
+    public List<Hole> getHoles() {
+        return this.holes;
+    }
+
+    public byte getNumHoles() {
+        return (byte) this.getHoles().size();
     }
 
 
